fac17acef56aa03c17e1282e7a0c566454d89078
Roomba324 <Philip.krbs2@gmail.com>
2025-06-10 21:37:33 +0200

    created player1 Object as a triangle centered on the screen

--- main.py
+++ main.py
@@ -3,20 +3,36 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
+
+clock = pygame.time.Clock()
+dt = 0
+
+
+ppx = SCREEN_WIDTH / 2
+ppy = SCREEN_HEIGHT / 2
+player1 = Player(ppx, ppy)
 
 def main():
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
 	while True:
 		for event in pygame.event.get():
     			if event.type == pygame.QUIT:
         			return
 		screen.fill("black")
+		dt = clock.tick(60)
+		dt /= 1000
+		player1.draw(screen)
 		pygame.display.flip()
 		
+
 	print("Starting Asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
+
